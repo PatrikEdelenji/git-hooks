8650fbc29ea3408e36b4e3dbddf96ccb8ff4b743
PatrikEdelenji <patrik.edelenji@gmail.com>
2022-12-08 13:29:51 +0100

    Reinstalled packages

--- anotherSQLfile.sql
+++ anotherSQLfile.sql
@@ -2,6 +2,19 @@
          ===========================================================
          HEADER
 
+         Author: <bound method Github.get_user of <github.MainClass.Github object at 0x0000021D9AC93FD0>>
+
+         Date: 08/12/2022
+
+         Description: This is sql file
+
+         ===========================================================
+*/
+         
+/*
+         ===========================================================
+         HEADER
+
          Author: PatrikEdelenji
 
          Date: 06/12/2022
